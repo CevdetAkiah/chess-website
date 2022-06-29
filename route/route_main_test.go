b3f0f924067a15292f68bc49d51e5d728a723e4f
Cevdet <cevdet.akiah@gmail.com>
2022-06-29 18:03:34 +0100

    expanded the TestRequest function to include all routes so far

--- route/route_main_test.go
+++ route/route_main_test.go
@@ -31,9 +31,37 @@ func setUp() {
 
 func TestRequest(t *testing.T) {
 	mux.HandleFunc("/", Request(&testServ))
-
 	if writer.Code != 200 {
 		t.Errorf("Response code is %v", writer.Code)
 	}
 
+	mux.HandleFunc("/signup", Request(&testServ))
+	if writer.Code != 200 {
+		t.Errorf("Response code is %v", writer.Code)
+	}
+
+	mux.HandleFunc("/errors", Request(&testServ))
+	if writer.Code != 200 {
+		t.Errorf("Response code is %v", writer.Code)
+	}
+
+	mux.HandleFunc("/login", Request(&testServ))
+	if writer.Code != 200 {
+		t.Errorf("Response code is %v", writer.Code)
+	}
+
+	mux.HandleFunc("/logout", Request(&testServ))
+	if writer.Code != 200 {
+		t.Errorf("Response code is %v", writer.Code)
+	}
+
+	mux.HandleFunc("/signupAccount", Request(&testServ))
+	if writer.Code != 200 {
+		t.Errorf("Response code is %v", writer.Code)
+	}
+
+	mux.HandleFunc("/authenticate", Request(&testServ))
+	if writer.Code != 200 {
+		t.Errorf("Response code is %v", writer.Code)
+	}
 }
