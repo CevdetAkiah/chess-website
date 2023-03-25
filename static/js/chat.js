e2eafc8b09de81dd8e09d12c708b1ad4b3b84173
CevdetAkiah <cevdet.akiah@gmail.com>
2023-03-25 15:36:02 +0000

    added in a break line after a certain message length to stop x axis overflow

--- static/js/chat.js
+++ static/js/chat.js
@@ -24,10 +24,15 @@ socket.onmessage = function(event){
     // parse message received from server
     let data = JSON.parse(event.data);
     let name = data.name;
-    let msg = data.message;
+    let msg = data.message + "<br>";
     // if message is longer than the length of the message box add a line break
-    if (msg.length > 40){
+    if (msg.length > 30){
         msgArray = msg.split("");
+        for (let i = 30; i < msg.length; i++){
+            if (i % 30 == 0) {
+                msgArray.splice(i,0,"<br>");
+            }
+        };
         msg = msgArray.join("")
     }
 
@@ -35,7 +40,7 @@ socket.onmessage = function(event){
     message = name.bold() + ":  " + msg;
     let msgcontainer = document.getElementById('messages');
     // append message to html message area
-    msgcontainer.innerHTML += message + "<br>";
+    msgcontainer.innerHTML += message;
 
     // auto scroll if overflow is reached
     msgcontainer.scrollTop = msgcontainer.scrollHeight;
