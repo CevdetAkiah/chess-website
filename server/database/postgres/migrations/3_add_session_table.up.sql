38aa3c9ef54d6ce2be9a0c7d5a5daf709c04cdcc
CevdetAkiah <33881176+CevdetAkiah@users.noreply.github.com>
2024-07-22 18:32:34 +0100

    fixed error in 3_add_session_table,up.sql (#104)

--- server/database/postgres/migrations/3_add_session_table.up.sql
+++ server/database/postgres/migrations/3_add_session_table.up.sql
@@ -2,6 +2,6 @@ CREATE TABLE IF NOT EXISTS chessapp.sessions (
   id         serial primary key,
   uuid       varchar(64) not null unique,
   email      varchar(255),
-  user_id    integer references users(id),
+  user_id    integer references chessapp.users(id),
   created_at timestamp not null   
 );
